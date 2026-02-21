18a5604aa9854c3bcb37657aef4d63e30f7dc44c
Ayush Jaiswal <aj7984435@gmail.com>
2026-02-21 16:28:55 +0530

    Added all files

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+  WalletModalProvider,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+import './App.css'
+import { RequestAirdrop } from './components/RequestAirdrop';
+import { ShowBalance } from './components/ShowBalance';
+
+function App() {
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+
+          <div className="main-container">
+
+            <div className="card">
+
+              <h1 className="title">
+                Solana Devnet Airdrop
+              </h1>
+
+              <p className="subtitle">
+                Connect your wallet and request SOL instantly
+              </p>
+
+              <WalletMultiButton className="wallet-btn"/>
+
+
+              <RequestAirdrop />
+              <ShowBalance />
+
+            </div>
+
+          </div>
+
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default App

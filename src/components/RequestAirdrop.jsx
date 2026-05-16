dc10bd297bdcddf5716c8371a6b25f1375ecf384
Ayush Jaiswal <aj7984435@gmail.com>
2026-05-16 14:30:05 +0530

    Added comment

--- src/components/RequestAirdrop.jsx
+++ src/components/RequestAirdrop.jsx
@@ -1,6 +1,6 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-
+// comment
 export function RequestAirdrop() {
 
     const wallet = useWallet();
